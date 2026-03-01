e11dc6e4303d27ac74fb5e358462b40f8815eb35
danielespaillat64 <daniel.espaillat@upr.edu>
2026-03-01 17:35:14 -0400

    Enemy respawn  after game over fix

--- src/Program.cpp
+++ src/Program.cpp
@@ -218,4 +218,23 @@ void Program::Reset() {
     delay = 0;
     lives = 3;
     score = 0;
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+            std::pair<float, float>{350, 150}, 
+            new SpEnemy(350, 150)
+        });
+
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+            std::pair<float, float>{600, 150}, 
+            new SpEnemy(600, 150)
+        });
+
+    for (int i = 0; i < 30; i++) {
+        float x = 250 + 50 * (i % 10);
+        float y = 200 + 50 * (i / 10);
+
+        Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+            std::pair<float, float>{x, y}, 
+            new StdEnemy(x, y)
+        });
+    }
 }
